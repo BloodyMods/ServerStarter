229b4699bd71a192b53dc1cc33bd2029caa15a43
Martmists <martmists@gmail.com>
2018-07-19 16:17:29 +0200

    Fix version
    Signed-off-by: Martmists <martmists@gmail.com>

--- src/main/resources/startserver.sh
+++ src/main/resources/startserver.sh
@@ -7,7 +7,7 @@ if [[ $(cat server-setup-config.yaml | grep 'ramDisk:' | awk 'BEGIN {FS=":"}{pri
     sudo mount -t ramfs -o size=8G ramfs $SAVE_DIR
     DO_RAMDISK=1
 fi
-java -d64 -jar serverstarter-1.2.4.jar
+java -d64 -jar serverstarter-@@serverstarter-libVersion@@.jar
 if [[ $DO_RAMDISK -eq 1 ]]; then
     sudo umount $SAVE_DIR
     rm -rf $SAVE_DIR
