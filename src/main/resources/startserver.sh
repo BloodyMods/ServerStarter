f796e1d9d2ac0a9d5496d2c5583afa7023730bdd
Martmists <martmists@gmail.com>
2018-07-19 12:21:32 +0200

    Minifix bash file
    Signed-off-by: Martmists <martmists@gmail.com>

--- src/main/resources/startserver.sh
+++ src/main/resources/startserver.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 DO_RAMDISK=0
-if [[ $(cat server-setup-config.yaml | grep 'ramDisk:' | awk 'BEGIN {FS=":"}{print $2}') =~ *"yes"* ]]; then
+if [[ $(cat server-setup-config.yaml | grep 'ramDisk:' | awk 'BEGIN {FS=":"}{print $2}') =~ "yes" ]]; then
     SAVE_DIR=$(cat server.properties | grep 'level-name' | awk 'BEGIN {FS="="}{print $2}')
     mv $SAVE_DIR "${SAVE_DIR}_backup"
     mkdir $SAVE_DIR
-    sudo mount -t tmpfs -o size=8G $SAVE_DIR
+    sudo mount -t tmpfs -o size=8G tmpfs $SAVE_DIR
     DO_RAMDISK=1
 fi
-java -d64 -jar serverstarter-@@serverstarter-libVersion@@.jar
+java -d64 -jar serverstarter-1.2.4.jar
 if [[ $DO_RAMDISK -eq 1 ]]; then
     sudo umount $SAVE_DIR
-    rmdir $SAVE_DIR
+    rm -rf $SAVE_DIR
     mv "${SAVE_DIR}_backup" $SAVE_DIR
 fi
