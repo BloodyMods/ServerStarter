3e3e2b7afd7e0e87c68c091c1c713ad8c1705201
BloodWorkXGaming <bloodworkxgaming@gmail.com>
2019-10-01 18:15:40 +0200

    updated sh

--- src/main/resources/startserver.sh
+++ src/main/resources/startserver.sh
@@ -7,6 +7,32 @@ if [[ $(cat server-setup-config.yaml | grep 'ramDisk:' | awk 'BEGIN {FS=":"}{pri
     sudo mount -t tmpfs -o size=2G tmpfs $SAVE_DIR
     DO_RAMDISK=1
 fi
+	if [ -f serverstarter-@@serverstarter-libVersion@@.jar ]; then
+			echo "Skipping download. Using existing serverstarter-@@serverstarter-libVersion@@.jar"
+         java -d64 -jar serverstarter-@@serverstarter-libVersion@@.jar
+               if [[ $DO_RAMDISK -eq 1 ]]; then
+               sudo umount $SAVE_DIR
+               rm -rf $SAVE_DIR
+               mv "${SAVE_DIR}_backup" $SAVE_DIR
+               fi
+               exit 0
+	else
+			export URL="https://github.com/AllTheMods/ServerStarter/releases/download/v@@serverstarter-libVersion@@/serverstarter-@@serverstarter-libVersion@@.jar"
+	fi
+		echo $URL
+		which wget >> /dev/null
+		if [ $? -eq 0 ]; then
+			echo "DEBUG: (wget) Downloading ${URL}"
+			wget -O serverstarter-@@serverstarter-libVersion@@.jar "${URL}"
+   else
+			which curl >> /dev/null
+			if [ $? -eq 0 ]; then
+				echo "DEBUG: (curl) Downloading ${URL}"
+				curl -o serverstarter-@@serverstarter-libVersion@@.jar "${URL}"
+			else
+				echo "Neither wget or curl were found on your system. Please install one and try again"
+         fi
+      fi
 java -d64 -jar serverstarter-@@serverstarter-libVersion@@.jar
 if [[ $DO_RAMDISK -eq 1 ]]; then
     sudo umount $SAVE_DIR
