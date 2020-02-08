f18ca25b4789e200f4313af82d58bf742a7af66d
BloodWorkXGaming <bloodworkxgaming@gmail.com>
2020-02-08 20:38:55 +0100

    change 1.1.1.1 to 1.0.0.1, closes #9

--- src/main/kotlin/atm/bloodworkxgaming/serverstarter/InternetManager.kt
+++ src/main/kotlin/atm/bloodworkxgaming/serverstarter/InternetManager.kt
@@ -15,7 +15,7 @@ object InternetManager {
             .readTimeout(30, TimeUnit.SECONDS)
             .build()!!
 
-    private val urls = listOf("8.8.8.8", "1.1.1.1")
+    private val urls = listOf("8.8.8.8", "1.0.0.1")
 
     fun checkConnection(): Boolean {
         var reached = 0
